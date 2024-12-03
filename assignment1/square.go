ae2f54ce229139950f98ab52069e72be5f088e61
Raramuri <apple@172-0-52-136.lightspeed.tukrga.sbcglobal.net>
2024-12-03 16:58:56 +0530

    added new function reactangle

--- assignment1/square.go
+++ assignment1/square.go
@@ -6,3 +6,6 @@ func Squareperimeter(sides int) {
 	//return 4 * sides formula for square perimeter
 	fmt.Printf("the perimeter of a square is:  %v\n", 4*sides)
 }
+func Reactangle(l, b int) {
+	fmt.Printf("the perimeter of a square is:  %v\n", 2*l+b)
+}
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/waheedsys/newproject/assignment1"
 
 func main() {
 	assignment1.Greet("waheed")
-
+	assignment1.Reactangle(2, 3)
 	assignment1.CirclePerimeter(4)
 	assignment1.Squareperimeter(4)
 }
