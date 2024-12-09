b64b4ee8647edbc3f33f06569fbae7aba95c31ae
syed waheed <apple@172-0-52-136.lightspeed.tukrga.sbcglobal.net>
2024-12-09 11:29:48 +0530

    refactored day1 package

---
+++ day1/assignment1/square.go
@@ -0,0 +1,19 @@
+package assignment1
+
+import "fmt"
+
+func Squareperimeter(sides int) {
+	//return 4 * sides formula for square perimeter
+	fmt.Printf("the perimeter of a square is:  %v\n", 4*sides)
+}
+func Reactangle(l, b int) {
+	fmt.Printf("the perimeter of a square is:  %v\n", 2*l+b)
+}
+
+func Cube(l, b, h int) {
+	fmt.Printf("the perimeter of a square is:  %v\n", l*b*h)
+}
+
+func Sphere(r float64) {
+	fmt.Printf("the perimeter of a square is:  %v\n", float64(4/3)*Pi*r*r*r)
+}
---
+++ day1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/waheedsys/day1/assignment1"
+)
+
+func main() {
+	assignment1.Greet("waheed")
+	assignment1.Reactangle(2, 3)
+	assignment1.CirclePerimeter(4)
+	assignment1.Squareperimeter(4)
+	assignment1.Cube(1, 2, 3)
+}
