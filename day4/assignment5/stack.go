9a055ee444a21febaefa8dfccdf0e4064e0ac200
syed waheed <apple@172-0-52-136.lightspeed.tukrga.sbcglobal.net>
2024-12-11 14:43:37 +0530

    changes in day4

--- day4/assignment5/stack.go
+++ day4/assignment5/stack.go
@@ -8,8 +8,9 @@ func pop(arr []int) int {
 	res := arr[len(arr)-1]
 	return res
 }
-func Stack(para int, arr []int) {
+func Stack(para int, arr []int) (s []int, err error) {
 	push(para, arr)
 	pop(arr)
+	return arr, nil
 
 }
